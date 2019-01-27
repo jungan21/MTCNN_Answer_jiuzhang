7ec389818e02bb5acabafcc0f069944ffb43257a
Jun Gan <jungan21@gmail.com>
2019-01-27 11:58:30 -0500

    Update prepare_pnet_data.sh

--- prepare_pnet_data.sh
+++ prepare_pnet_data.sh
@@ -2,10 +2,14 @@
 
 cd prepare_data
 
+# face detection data
 python3 gen_12net_data.py
 
+# landmark data
 python3 gen_landmark_aug_12.py
 
+# meage above 2 together
 python3 gen_imglist_pnet.py
 
+# convert to TF records data
 python3 gen_PNet_tfrecords.py
